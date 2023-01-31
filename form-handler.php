77e2889160e50eabab247aa090519103dc258eb3
Zouhair <121346419+ICH-BIN-ZOUHAIR@users.noreply.github.com>
2023-02-01 00:55:50 +0100

    Add files via upload

---
+++ form-handler.php
@@ -0,0 +1,33 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subgect = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_from = 'info@foodeliveryzouhair.000webhostapp.com';
+
+$email_subject = 'New Form Submission';
+
+$email_body = "User Name: $name.\n".
+                "User email: $visitor_email.\n".
+                    "Subject: $subject.\n".
+                    "User Message: $message.\n";
+
+$to = 'zogamaouddach@gmail.com';
+
+$headers = "from: $email_from \r\n";
+
+$headers .= "Reply-To: $visitor_email \r\n";
+
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("location: contact");
+?>
+
+
+
+
+
+
+
